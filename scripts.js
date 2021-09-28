50cf25f86ac57760e90493fea44c8878a9c3d6e6
alaJream <jeremy.gonzales@students.dominican.edu>
2021-09-28 09:51:44 -0700

    added back button function for another tip

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
 function caculateTip() {
     var price = doucment.getElementById("total").value;
-    var queantity = document.getElementById("quantity").value;
+    var quantity = document.getElementById("quantity").value;
     var satisfaction = doucment.getElementById("satisfaction").value;
 
     var tip = ((price * satisfaction) / quantity);
 
+    if(price === "" || price === "0") {
+        alert("Please Enter Valid Price");
+    }   else if(quantity === "" || quantity === "0") {
+        alert("Please Enter Valid Number Of People");
 
+    }   else {
+        var front = document.getElementById("front");
+    front.style.display = "none";
+    var back = document.getElementById("back")
+    back.style.display = "block";
+
+    document.getElementById("tip").innerHTML = ("Your tip split between " + quantity + " person(s) is $" + tip.toFixed(2)); 
+    }
+}
+
+function back() {
+    var front = document.getElementById("front");
+    front.style.display = "block";
+    var back = document.getElementById("back");
+    back.style.display = "none";
+
+    document.getElementById("myform").reset();
+    
 }
