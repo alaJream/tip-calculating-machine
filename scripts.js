50abb228a6e5ff824dcb60c70f5697878f04151c
alaJream <jeremy.gonzales@students.dominican.edu>
2021-09-28 12:14:05 -0700

    fixed error of stylesheet not displaying

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-function caculateTip() {
-    var price = doucment.getElementById("total").value;
-    var quantity = document.getElementById("quantity").value;
-    var satisfaction = doucment.getElementById("satisfaction").value;
-
-    var tip = ((price * satisfaction) / quantity);
-
-    if(price === "" || price === "0") {
-        alert("Please Enter Valid Price");
-    }   else if(quantity === "" || quantity === "0") {
-        alert("Please Enter Valid Number Of People");
-
-    }   else {
-        var front = document.getElementById("front");
-    front.style.display = "none";
-    var back = document.getElementById("back")
-    back.style.display = "block";
-
-    document.getElementById("tip").innerHTML = ("Your tip split between " + quantity + " person(s) is $" + tip.toFixed(2)); 
-    }
-}
-
-function back() {
+function calculateTip(){
+  var price = document.getElementById("total").value;
+  var quantity = document.getElementById("quantity").value;
+  var satisfaction = document.getElementById("satisfaction").value;
+  
+  var tip = ((price * satisfaction) / quantity);
+   
+  if(price === "" || price === "0") {
+    alert("Please enter a valid price");
+  } else if(quantity === "" || quantity === "0") {
+    alert("Please enter a valid number of people");
+            
+  } else {
     var front = document.getElementById("front");
-    front.style.display = "block";
-    var back = document.getElementById("back");
-    back.style.display = "none";
+  front.style.display = "none";
+  var back = document.getElementById("back");
+  back.style.display = "block";
+  
+  document.getElementById("tip").innerHTML = ("Your tip split between " + quantity + " person(s) is $" + tip.toFixed(2));
+  }
+}
+  
+function back() {
+  var front = document.getElementById("front");
+  front.style.display = "block";
+  var back = document.getElementById("back");
+  back.style.display = "none";
+  
+  document.getElementById('myform').reset();
 
-    document.getElementById("myform").reset();
-    
 }
